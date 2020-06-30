e7bdbf11bbb9d5ff740c5686b3b4b72f78d39a37
shahab-qazavi <shahab.geek@gmail.com>
2020-06-30 17:42:21 +0430

    going to fix text_reader

--- daemons/text_reader.py
+++ daemons/text_reader.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-print('KOOOOOOOOOOOOOOOOOOOOON')
 import sys
-sys.path.append('/home/ehsan/dev/newshub')
+sys.path.append('/home/oem/dev/newshub')
 sys.path.append('/root/dev/newshub')
 import requests
 from bs4 import BeautifulSoup
 from publics import db, es, PrintException
 from datetime import datetime
 from bson import ObjectId
+from queue import Queue
+import threading
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 col_news = db()['news']
-# col_engine_logs = db()['engine_logs']
 col_engine_instances = db()['engine_instances']
 col_error_logs = db()['error_logs']
 col_source_links = db()['source_links']
 news_id = ''
 if len(sys.argv) > 1:
     news_id = sys.argv[1]
-print(news_id)
-
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-from queue import Queue
-import threading
 q = Queue()
 thread_count = 30
+count = 0
+
 
 def log(type, page_url, selector, data, error, source_id, engine_instance_id):
     try:
@@ -53,28 +50,26 @@ def remove_hrefs(html):
 
 
 def do_work(item):
-    # try:
+        global count
+        count+=1
+        print(count)
         news_html = None
         item['mongo_id'] = str(item['_id'])
         del item['_id']
-        # print(item['url'])
-        result = requests.get(item['url'], verify=False)
-        # print(result)
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        result = requests.get(item['url'], headers=headers, verify=False)
         html = BeautifulSoup(result.text, 'html.parser')
         try:
-            news_html = html.select(item['text_selector'])[0]
+            news_html = html.select(item['text_selector'])
+            if len(news_html) != 0: news_html = news_html[0]
             news_html = remove_hrefs(news_html)
 
             status = 'text'
             source_link_info = col_source_links.find_one({'_id': ObjectId(item['source_link_id'])})
-            # with open("temp.html", "w") as file:
-            #     file.write(str(news_html))
             if 'exclude' in source_link_info:
                 for exclude in source_link_info['exclude']:
                     for ex in news_html.select(exclude):
                         ex.decompose()
-            # with open("tempdec.html", "w") as file:
-            #     file.write(str(news_html))
         except Exception as e:
             news_html = ''
             status = 'error_text'
@@ -85,17 +80,16 @@ def do_work(item):
                 'text': '',
                 'html': '',
                 'error': str(e),
-                # 'error_details': {'url': item['url']},
             }})
-
         if news_html is not None and news_html != '':
-            # print(type(news_html))
-            # print(news_html)
             news_text = news_html.text
             item['status'] = status
             item['text'] = news_text
             item['html'] = str(news_html)
             item['text_reader_id'] = engine_instance_id
+            print('+++++++++++++++++++++++++')
+            print(len(news_text))
+            print('+++++++++++++++++++++++++')
             es().index(index='newshub', doc_type='news', body=item)
             col_news.update_one({'_id': ObjectId(item['mongo_id'])}, {'$set': {
                 'status': status,
@@ -103,32 +97,17 @@ def do_work(item):
                 'html': str(news_html),
             }})
 
-    # except Exception as e:
-    #     print('FUCKED UP')
-    #     print(e)
-    #     PrintException()
-    #     col_news.update_one({'_id': ObjectId(item['mongo_id'])}, {'$set': {
-    #         'status': 'error',
-    #         'text': '',
-    #         'html': news_html,
-    #         'error': str(e),
-    #     }})
-    #
-    #     log(type='read_url', page_url=item['url'], selector='', data={}, error=PrintException(),
-    #         engine_instance_id=engine_instance_id, source_id=item['source_id'])
-
 
 def worker():
     while True:
         item = q.get()
         do_work(item)
-        q.task_done()
+        print(q.task_done())
 
 
 def run():
     if news_id == '':
         news_list = col_news.find({'status': 'summary'}).sort('create_date', -1)
-        # print(news_list.count())
 
     else:
         news_list = col_news.find({'_id': ObjectId(news_id)})
@@ -156,12 +135,9 @@ engine_instance_id = str(col_engine_instances.insert_one({
 }).inserted_id)
 error_count = 0
 new_contents = 0
-# source_id = ''
-# if len(sys.argv) > 1:
-#     print(sys.argv[1])
-#     source_id = sys.argv[1]
 run()
 duration = (datetime.now() - start).total_seconds()
+print(duration)
 
 col_engine_instances.update_one({'_id': ObjectId(engine_instance_id)}, {'$set': {
     'duration': duration,
@@ -169,5 +145,3 @@ col_engine_instances.update_one({'_id': ObjectId(engine_instance_id)}, {'$set': 
     'source_links': source_links,
     'new_contents': ''
 }})
-print(duration)
-
