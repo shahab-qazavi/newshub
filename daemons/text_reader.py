faa239bac6c9400c48eb928dca6541e01c91e0b0
shahab <shahab.geek@gmail.com>
2020-07-19 01:04:33 +0430

    fix some bugs

--- daemons/text_reader.py
+++ daemons/text_reader.py
@@ -60,8 +60,7 @@ def do_work(item):
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         try:
             result = requests.get(item['url'], headers=headers, verify=False)
-        except requests.exceptions.ConnectionError as e:
-            print(item['url'])
+        except:
             result = requests.get(item['url'], verify=False)
         # except:
         #     result = requests.get(item['url'])
