b270d41c85ea0452a273a89de055a25ac5bb0fb2
shahab <shahab.geek@gmail.com>
2020-07-19 22:33:45 +0430

    fix some bugs

--- daemons/text_reader.py
+++ daemons/text_reader.py
@@ -63,12 +63,14 @@ def do_work(item):
         except requests.exceptions.ConnectionError:
             try:
                 result = requests.get(item['url'], verify=False)
-            except Exception as e:
-                result = None
-                log(type='read_text', page_url=item['url'], selector='', data={},
-                    error=PrintException(), engine_instance_id=engine_instance_id, source_id=item['source_id'])
-        except:
-            result = requests.get(item['url'])
+            except:
+                try:
+                    result = requests.get(item['url'])
+                except Exception as e:
+                    result = None
+                    log(type='read_text', page_url=item['url'], selector='', data={},
+                        error=e, engine_instance_id=engine_instance_id, source_id=item['source_id'])
+
 
         status = ''
         if result != '' or result is not None:
