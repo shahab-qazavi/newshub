5d1a089a17bd056d6eb3d40f2f89b906b3f0fb02
shahab <shahab.geek@gmail.com>
2020-07-20 00:00:24 +0430

    fix some bugs

--- daemons/text_reader.py
+++ daemons/text_reader.py
@@ -66,7 +66,7 @@ def do_work(item):
             try:
                 result = requests.get(item['url'], verify=False)
             except:
-                result = None
+                result = ''
                 log(type='read_text', page_url=item['url'], selector='', data={},
                     error=PrintException(), engine_instance_id=engine_instance_id, source_id=item['source_id'])
 
