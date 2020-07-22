6bafef277f04958d938658969b34a9cc17b9334f
shahab <shahab.geek@gmail.com>
2020-07-23 00:01:19 +0430

    fix some bugs

--- daemons/text_reader.py
+++ daemons/text_reader.py
@@ -81,12 +81,14 @@ def do_work(item):
             try:
                 html = BeautifulSoup(result.text, 'html.parser')
             except:
+                html = ''
                 print('--------------')
                 ddd = [result, '']
                 print(type(result))
                 print(ddd)
             try:
-                news_html = html.select(item['text_selector'])
+                if html != '' or html is not None:
+                    news_html = html.select(item['text_selector'])
                 if len(news_html) > 0:
                     news_html = news_html[0]
                     try:
@@ -99,10 +101,10 @@ def do_work(item):
                         for exclude in source_link_info['exclude']:
                             for ex in news_html.select(exclude):
                                 ex.decompose()
-                # else:
-                #     status = 'Empty'
-                #     log(type='read_text', page_url=item['url'], selector=item['text_selector'], data={},
-                #         error='Empty', engine_instance_id=engine_instance_id, source_id=item['source_id'])
+                else:
+                    status = 'Empty'
+                    log(type='read_text', page_url=item['url'], selector=item['text_selector'], data={},
+                        error='Empty', engine_instance_id=engine_instance_id, source_id=item['source_id'])
             except Exception as e:
                 news_html = ''
                 status = 'error_text'
