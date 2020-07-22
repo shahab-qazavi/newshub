3c45b6173f4b6fa502f91177dde65747671b873a
shahab <shahab.geek@gmail.com>
2020-07-23 00:05:38 +0430

    fix some bugs

--- daemons/text_reader.py
+++ daemons/text_reader.py
@@ -128,11 +128,11 @@ def do_work(item):
             item['text'] = news_text
             item['html'] = str(news_html)
             item['text_reader_id'] = engine_instance_id
-            try:
-                es().index(index='newshub', doc_type='news', body=item)
-            except:
-                run(['systemctl','restart','elasticsearch'])
-                es().index(index='newshub', doc_type='news', body=item)
+            # try:
+            #     es().index(index='newshub', doc_type='news', body=item)
+            # except:
+            #     run(['systemctl','restart','elasticsearch'])
+            #     es().index(index='newshub', doc_type='news', body=item)
             col_news.update_one({'_id': ObjectId(item['mongo_id'])}, {'$set': {
                 'status': status,
                 'text': news_text,
