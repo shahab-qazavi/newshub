40e8687bd6b0d337143e5a54b5e7b0900e0e4791
shahab <55015934+shahab-qazavi@users.noreply.github.com>
2020-07-30 16:19:25 +0430

    Update handlers.py

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -475,7 +475,7 @@ class Home(BaseHandler):
                 if self.params.get('header') == 'header':
                     print(1)
                     # print()
-                    news = self.prepare_dataset(col_news.find({'summary': {'$ne':''} , 'text': {'$ne': ''}}).limit(1).sort('create_date',-1))
+                    news = self.prepare_dataset(col_news.find({'summary': {'$ne': ''} , 'text': {'$ne': ''}}).limit(1).skip(randint(1,10)).sort('create_date',-1))
                     print(news[0])
                     self.output['data']['item']['header_news'] = [{
                         'mongo_id': news[0]['id'],
